2fc3da5cc97eb48b25d1fcd00ee6e4d1fb064ec2
Nereol <pimboliwell.fr@gmail.com>
2025-03-21 17:01:02 +0100

    ajout du front

--- api/themealDB/whats_in_my_fridge.js
+++ api/themealDB/whats_in_my_fridge.js
@@ -2,6 +2,49 @@ import { getListIngredients } from '/scripts/ingredient.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const buttonGenerate = document.getElementById('btnGenerateInMyFridge');
+  const container = document.getElementById('recipe_container');
+
+	function createRecipeCard(meal) {
+		const card = document.createElement('div');
+		card.className = 'flex items-center gap-2 m-4';
+
+		const img = document.createElement('img');
+		img.src = meal.strMealThumb;
+		img.alt = meal.strMeal;
+		img.className = 'w-32 h-32';
+		card.appendChild(img);
+
+		const content = document.createElement('div');
+		content.className = 'pl-10 flex flex-col self-center gap-4';
+
+		const bookmarkButton = document.createElement('button');
+		bookmarkButton.className = 'text-black self-end';
+		bookmarkButton.innerHTML = '<i class="fa-regular fa-bookmark"></i>';
+		content.appendChild(bookmarkButton);
+
+		const description = document.createElement('p');
+		description.className = 'w-3/4 text-lg font-bold';
+		description.textContent = meal.strMeal;
+		content.appendChild(description);
+
+		const buttonsContainer = document.createElement('div');
+		buttonsContainer.className = 'flex gap-2';
+
+		content.appendChild(buttonsContainer);
+		card.appendChild(content);
+
+		return card;
+	}
+
+	async function displayMeals(ingredients) {
+		const meals = await getMealFromIngredients(ingredients);
+		container.innerHTML = '';
+		meals.forEach(meal => {
+			const recipeCard = createRecipeCard(meal);
+			container.appendChild(recipeCard);
+		});
+	}
+
 
   async function getMealFromIngredients(ingredients) {
     try {
@@ -26,6 +69,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const filteredMeals = meals.filter(meal => mealsCount[meal.idMeal] === ingredients.length);
 
+	  if (filteredMeals.length === 0) {
+        console.log(`Aucun plat trouvé pour les ingrédients : ${ingredients.join(", ")}`);
+        return [];
+      }
+
       console.log(`Plats contenant ${ingredients.join(", ")} :`, filteredMeals);
       return filteredMeals;
     } catch (error) {
@@ -38,5 +86,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const listIngredients = getListIngredients(); // 🔄 Récupérer la liste mise à jour
     console.log("Ingrédients sélectionnés :", listIngredients);
     getMealFromIngredients(listIngredients);
+	displayMeals(listIngredients);
   });
 });
