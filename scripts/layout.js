ecd2239cbe7f05ae6f4a744971fecb4335dab038
Nereol <pimboliwell.fr@gmail.com>
2025-03-23 21:04:51 +0100

    oups

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,7 +1,7 @@
 // Fonction pour charger le header et le footer
 function loadHeaderFooter() {
     // Charger le header
-    fetch('../pages/template/header.html')
+    fetch('../pages/header.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
